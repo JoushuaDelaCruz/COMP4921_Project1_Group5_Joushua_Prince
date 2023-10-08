4f0e89cac8fd483336dbe81f36c7e8fdf185982d
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-07 18:33:05 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+require("./utils");
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("404 Not Found");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./views/*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
