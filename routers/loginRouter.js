4dc659ef2aab91e65fcaeeaff2f8d23835b4ccf9
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-12 21:39:39 -0700

    Renamed files by adding `Routers` at the end

---
+++ routers/loginRouter.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const expireTime = 60 * 60 * 1000; //expires after 1 day  (hours * minutes * seconds * millis)
+
+router.get("/", function (req, res) {
+  res.render("login");
+});
+
+router.post("/", function (req, res) {
+  console.log(req.body);
+  res.send("POST request to the homepage");
+});
+
+module.exports = router;
