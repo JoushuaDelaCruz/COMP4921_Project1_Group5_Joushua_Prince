513a6307ec0e766ff2624539b2e3e8e16c9e476d
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-16 21:07:03 -0700

    Implemented getPosts

--- database/db_posts.js
+++ database/db_posts.js
@@ -20,4 +20,32 @@ const create = async (post) => {
   }
 };
 
-module.exports = { create };
+const getPosts = async () => {
+  const query = `
+  SELECT 
+	content_id, 
+    u.user_id, 
+    num_views, 
+    username, 
+    profile_img, 
+    post_id, 
+    title, 
+    content, 
+    date_created, 
+    COUNT(vote_id) as num_votes
+  FROM contents
+  JOIN posts USING (content_id)
+  JOIN users AS u USING (user_id)
+  LEFT JOIN votes USING (content_id)
+  GROUP BY content_id
+  `;
+  try {
+    const result = await database.query(query);
+    return result[0];
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+module.exports = { create, getPosts };
