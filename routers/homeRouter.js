f059da1f770c38ac50646d4d7ac238e52e091418
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-16 21:07:21 -0700

    Added getting of posts

--- routers/homeRouter.js
+++ routers/homeRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const db_post = include("database/db_posts");
 
-router.get("/", function (req, res) {
-  res.render("home");
+router.get("/", async (req, res) => {
+  const posts = await db_post.getPosts();
+  res.render("home", { posts: posts });
 });
 
 module.exports = router;
