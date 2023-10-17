15a76d1c0c0c7398fbfd8a3356b3ce81076cd081
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-16 19:50:51 -0700

    Added a create route

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const db_post = include("database/db_posts");
 
 router.get("/", function (req, res) {
   res.render("post");
+});
+
+router.post("/create", async (req, res) => {
+  console.log(req.body);
+  const post = {
+    user_id: 1,
+    content: req.body.content,
+    parent_id: null,
+    title: req.body.title,
+  };
+  const successful = await db_post.create(post);
+  res.send(successful);
 });
 
 module.exports = router;
