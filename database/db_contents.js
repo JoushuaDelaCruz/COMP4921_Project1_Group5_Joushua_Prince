9583ff365bdba18dce8f8beddad2c6d7ad45e018
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-16 19:49:35 -0700

    Created a create function

--- database/db_contents.js
+++ database/db_contents.js
@@ -1,1 +1,24 @@
 const database = include("mySQLDatabaseConnection");
+
+const create = async (post) => {
+  const query = `
+          INSERT INTO contents (user_id, content, parent_id, date_created)
+          VALUES (:user_id, :content, :parent_id, :date_created)
+      `;
+  const params = {
+    user_id: post.user_id,
+    content: post.content,
+    parent_id: post.parent_id,
+    date_created: new Date(),
+  };
+  try {
+    const result = await database.query(query, params);
+    console.log(result[0]);
+    return result[0].insertId;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+module.exports = { create };
