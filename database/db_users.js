fe1d4c3f060126059d259f3f879a437c43a01da3
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-17 22:14:03 -0700

    Added create, isUsernameExist and isEmailExists

--- database/db_users.js
+++ database/db_users.js
@@ -1,1 +1,57 @@
 const database = include("mySQLDatabaseConnection");
+
+const create = async (user) => {
+  const query = `
+        INSERT INTO users (username, email, password, profile_img)
+        VALUES (:username, :email, :password, :image)
+    `;
+  const params = {
+    username: user.username,
+    password: user.password,
+    email: user.email,
+    image: user.image,
+  };
+  try {
+    await database.query(query, params);
+    return true;
+  } catch (error) {
+    console.error("Error while creating user:", error);
+    return false;
+  }
+};
+
+const isUsernameExist = async (username) => {
+  const query = `
+        SELECT username
+        FROM users
+        WHERE username = :username
+  `;
+  const params = { username: username };
+  try {
+    const result = await database.query(query, params);
+    console.log("result:", result);
+    return result[0].length > 0;
+  } catch (error) {
+    console.error("Error while checking username:", error);
+    return false;
+  }
+};
+
+const isEmailExist = async (email) => {
+  const query = `
+        SELECT email
+        FROM users
+        WHERE email = :email
+  `;
+  const params = { email: email };
+  try {
+    const result = await database.query(query, params);
+    console.log("result:", result);
+    return result[0].length > 0;
+  } catch (error) {
+    console.error("Error while checking email:", error);
+    return false;
+  }
+};
+
+module.exports = { create, isUsernameExist, isEmailExist };
