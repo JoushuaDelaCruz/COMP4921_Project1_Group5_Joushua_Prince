327309bc9f5b78fddf0ea1a922f2f134a359f625
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-18 21:00:01 -0700

    Changed to send the posts

--- routers/homeRouter.js
+++ routers/homeRouter.js
@@ -4,7 +4,7 @@ const db_post = include("database/db_posts");
 
 router.get("/", async (req, res) => {
   const posts = await db_post.getPosts();
-  res.render("home", { posts: posts });
+  res.send(posts);
 });
 
 module.exports = router;
