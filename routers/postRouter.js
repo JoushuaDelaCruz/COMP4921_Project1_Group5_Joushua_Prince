d44a969ca533d66be4d64c324c4a319ab39f0c23
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-18 22:09:02 -0700

    Added posts get request

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const db_post = include("database/db_posts");
 
-router.get("/", (req, res) => {
-  res.render("post");
+router.get("/", async (req, res) => {
+  const posts = await db_post.getPosts();
+  res.send(posts);
 });
 
 router.post("/create", async (req, res) => {
