650f8609ca89f2eb45bba6d6c2f3cb454bc77bc5
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-20 17:52:26 -0700

    Cleaned ejs code and comment

--- server.js
+++ server.js
@@ -15,25 +15,18 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const expireTime = 60 * 60 * 1000; //expires after 1 hr (hours * minutes * seconds * millis)
 
-//** MongoDB Session */
-/* secret information section */
 const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
-
 const node_session_secret = process.env.NODE_SESSION_SECRET;
-/* END secret section */
 
 const mongoStore = MongoStore.create({
   mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@cluster0.nbfzg7h.mongodb.net/?retryWrites=true&w=majority`,
