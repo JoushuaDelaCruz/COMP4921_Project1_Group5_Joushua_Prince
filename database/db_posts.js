89ed9514173c4caecd8b9ee5bdacfcc0eac7f325
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-22 23:25:10 -0700

    Now gets the insertId of the newly created content

--- database/db_posts.js
+++ database/db_posts.js
@@ -2,7 +2,8 @@ const database = include("mySQLDatabaseConnection");
 const db_content = include("database/db_contents");
 
 const create = async (post) => {
-  const content_id = await db_content.create(post);
+  const content = await db_content.create(post);
+  const content_id = content.insertId;
   const query = `
     INSERT INTO posts (content_id, title)
     VALUES (:content_id, :title)
@@ -20,6 +21,43 @@ const create = async (post) => {
   }
 };
 
+const getPost = async (post_id) => {
+  const query = `
+  WITH RECURSIVE cte_posts AS 
+	( SELECT content_id, user_id, content, date_created, content_id AS parent
+	  FROM contents WHERE parent_id IS NULL
+      UNION
+      SELECT c.content_id, c.user_id, c.content, c.date_created, cte.parent
+      FROM cte_posts cte
+      JOIN contents c ON cte.content_id = c.parent_id
+    )
+  SELECT 
+    cte.content_id,
+    cte.user_id,
+    username,
+    profile_img,
+    title,
+    date_created,
+    content,
+    COUNT(*) - 1 AS num_comments
+  FROM cte_posts cte
+  JOIN users USING (user_id)
+  JOIN posts p ON p.content_id = cte.parent
+  WHERE cte.content_id = :post_id
+  GROUP BY parent;
+  `;
+  const params = {
+    post_id: post_id,
+  };
+  try {
+    const result = await database.query(query, params);
+    return result[0];
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
+
 const getPosts = async () => {
   const query = `
   WITH RECURSIVE cte_posts AS 
@@ -33,7 +71,7 @@ const getPosts = async () => {
   SELECT 
       cte.content_id,
       cte.user_id,
-      cte.parent,
+      username,
       profile_img,
       title,
       date_created,
@@ -54,4 +92,4 @@ const getPosts = async () => {
   }
 };
 
-module.exports = { create, getPosts };
+module.exports = { create, getPosts, getPost };
