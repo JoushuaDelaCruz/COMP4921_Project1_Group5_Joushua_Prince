25fac0762f39519e35f708a0f35087dc1110499b
Prince H Chabveka <pchabveka@my.bcit.ca>
2023-10-30 22:25:20 -0700

    beautify

--- database/db_search.js
+++ database/db_search.js
@@ -3,11 +3,16 @@ const database = include("mySQLDatabaseConnection");
 
 const searchComments = async (keyword) => {
     const query = `
-    SELECT text, 
-    MATCH(text) AGAINST (:keyword IN BOOLEAN MODE) as score
-FROM comments
-WHERE MATCH(text) AGAINST (:keyword IN BOOLEAN MODE)
-ORDER BY score DESC;
+    SELECT 
+  text, 
+  MATCH(text) AGAINST (: keyword IN BOOLEAN MODE) as score 
+FROM 
+  comments 
+WHERE 
+  MATCH(text) AGAINST (: keyword IN BOOLEAN MODE) 
+ORDER BY 
+  score DESC;
+
 
           
     `;
