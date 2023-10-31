490df19ad7057f0f93bd90784cb598b3865e4bc4
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-30 20:34:21 -0700

    Added getPostsAndUserVotes and to show number of votes per posts

--- database/db_posts.js
+++ database/db_posts.js
@@ -60,26 +60,28 @@ const getPost = async (post_id) => {
 const getPosts = async () => {
   const query = `
   WITH RECURSIVE cte_posts AS 
-	  (SELECT content_id, user_id, content, date_created, content_id AS parent
-	  FROM contents WHERE parent_id IS NULL
-      UNION
-      SELECT c.content_id, c.user_id, c.content, c.date_created, cte.parent
-      FROM cte_posts cte
-      JOIN contents c ON cte.content_id = c.parent_id
-    )
-  SELECT 
-      cte.content_id,
-      cte.user_id,
-      username,
-      profile_img,
-      title,
-      date_created,
-      content,
-      COUNT(*) - 1 AS num_comments
+  (SELECT content_id, user_id, content, date_created, content_id AS parent
+  FROM contents WHERE parent_id IS NULL
+    UNION
+    SELECT c.content_id, c.user_id, c.content, c.date_created, cte.parent
+    FROM cte_posts cte
+    JOIN contents c ON cte.content_id = c.parent_id
+  )
+SELECT 
+    cte.content_id,
+    cte.user_id,
+    username,
+    profile_img,
+    title,
+    date_created,
+    content,
+    COUNT(*) - 1 AS num_comments,
+    IFNULL(SUM(v.value), 0) AS num_votes
   FROM cte_posts cte
   JOIN users USING (user_id)
   LEFT JOIN posts p ON p.content_id = cte.parent
-  GROUP BY parent
+  LEFT JOIN votes v ON v.content_id = cte.content_id
+  GROUP BY parent, user_id
   ORDER BY date_created DESC
   `;
   try {
@@ -91,4 +93,52 @@ const getPosts = async () => {
   }
 };
 
-module.exports = { create, getPosts, getPost };
+const getPostsAndUserVotes = async (user_id) => {
+  const query = `
+  WITH RECURSIVE cte_posts AS 
+  (SELECT content_id, user_id, content, date_created, content_id AS parent
+  FROM contents WHERE parent_id IS NULL
+    UNION
+    SELECT c.content_id, c.user_id, c.content, c.date_created, cte.parent
+    FROM cte_posts cte
+    JOIN contents c ON cte.content_id = c.parent_id
+  )
+SELECT 
+    cte.content_id,
+    cte.user_id,
+    username,
+    profile_img,
+    title,
+    date_created,
+    content,
+    COUNT(*) - 1 AS num_comments,
+    IFNULL(SUM(v.value), 0) AS num_votes,
+    user_votes.vote_id,
+    user_votes.value AS vote_value
+  FROM cte_posts cte
+  JOIN users USING (user_id)
+  LEFT JOIN posts p ON p.content_id = cte.parent
+  LEFT JOIN votes v ON v.content_id = cte.content_id
+  LEFT JOIN votes user_votes ON user_votes.user_id = :user_id AND user_votes.content_id = cte.content_id
+  GROUP BY parent, cte.user_id
+  ORDER BY date_created DESC;
+  `;
+  const params = {
+    user_id: user_id,
+  };
+
+  try {
+    const result = await database.query(query, params);
+    return result[0];
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
+
+module.exports = {
+  create,
+  getPosts,
+  getPost,
+  getPostsAndUserVotes,
+};
