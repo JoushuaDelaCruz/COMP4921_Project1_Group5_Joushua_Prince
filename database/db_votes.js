3b5944cbeb94404e79d881e2974060eb4a2b0b4b
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-30 20:34:46 -0700

    Created toggle function

--- database/db_votes.js
+++ database/db_votes.js
@@ -1,1 +1,23 @@
 const database = include("mySQLDatabaseConnection");
+
+const toggle = (vote_id, vote_value) => {
+  const query = `
+    UPDATE votes
+    SET value = :toggle_value
+    WHERE vote_id = :vote_id
+    `;
+  const params = {
+    vote_id: vote_id,
+    toggle_value: vote_value,
+  };
+  console.log("toggle");
+  try {
+    database.query(query, params);
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
+
+module.exports = { upVote, unvote, downVote, toggle };
