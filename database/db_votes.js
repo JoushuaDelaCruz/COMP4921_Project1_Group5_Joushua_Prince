122f0a2a6d79f0b87e6c90c439e96aec4cad1e43
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-31 18:23:55 -0700

    Implemented record function to insert new vote

--- database/db_votes.js
+++ database/db_votes.js
@@ -1,18 +1,20 @@
 const database = include("mySQLDatabaseConnection");
+const UP_VOTE = 1;
+const DOWN_VOTE = -1;
 
-const toggle = (vote_id, vote_value) => {
+const toggle = async (vote_id, vote_orig) => {
   const query = `
     UPDATE votes
     SET value = :toggle_value
     WHERE vote_id = :vote_id
     `;
+  const toggled_value = vote_orig === UP_VOTE ? DOWN_VOTE : UP_VOTE;
   const params = {
     vote_id: vote_id,
-    toggle_value: vote_value,
+    toggle_value: toggled_value,
   };
-  console.log("toggle");
   try {
-    database.query(query, params);
+    await database.query(query, params);
     return true;
   } catch (err) {
     console.log(err);
@@ -20,4 +22,23 @@ const toggle = (vote_id, vote_value) => {
   }
 };
 
-module.exports = { toggle };
+const record = async (content_id, user_id, is_up_vote) => {
+  const query = `
+    INSERT INTO votes (content_id, user_id, value)
+    VALUES (:content_id, :user_id, :value);
+  `;
+  const params = {
+    content_id: content_id,
+    user_id: user_id,
+    value: is_up_vote ? UP_VOTE : DOWN_VOTE,
+  };
+  try {
+    const result = await database.query(query, params);
+    return result[0].insertId;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+};
+
+module.exports = { toggle, record };
