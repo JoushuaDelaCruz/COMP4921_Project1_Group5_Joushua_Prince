3c441c58f3ceefc1394623640ac3a08f1c2f5242
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-31 21:08:52 -0700

    Fixed the vote_count retrieval bug

--- database/db_posts.js
+++ database/db_posts.js
@@ -60,29 +60,37 @@ const getPost = async (post_id) => {
 const getPosts = async () => {
   const query = `
   WITH RECURSIVE cte_posts AS 
-  (SELECT content_id, user_id, content, date_created, content_id AS parent
-  FROM contents WHERE parent_id IS NULL
-    UNION
-    SELECT c.content_id, c.user_id, c.content, c.date_created, cte.parent
-    FROM cte_posts cte
-    JOIN contents c ON cte.content_id = c.parent_id
+  (
+      SELECT content_id, user_id, content, date_created, content_id AS parent
+      FROM contents 
+      WHERE parent_id IS NULL
+      UNION
+      SELECT c.content_id, c.user_id, c.content, c.date_created, cte.parent
+      FROM cte_posts cte
+      JOIN contents c ON cte.content_id = c.parent_id
+  ), vote_counts AS
+  (
+      SELECT SUM(v.value) AS num_votes, cte.content_id
+      FROM cte_posts cte
+      LEFT JOIN votes v ON cte.content_id = v.content_id
+      GROUP BY cte.content_id
   )
-SELECT 
-    cte.content_id,
-    cte.user_id,
-    username,
-    profile_img,
-    title,
-    date_created,
-    content,
-    COUNT(*) - 1 AS num_comments,
-    IFNULL(SUM(v.value), 0) AS num_votes
+  SELECT 
+      cte.content_id,
+      cte.user_id,
+      username,
+      profile_img,
+      title,
+      date_created,
+      content,
+      COUNT(*) - 1 AS num_comments,
+      IFNULL(v.num_votes, 0) AS num_votes
   FROM cte_posts cte
   JOIN users USING (user_id)
   LEFT JOIN posts p ON p.content_id = cte.parent
-  LEFT JOIN votes v ON v.content_id = cte.content_id
-  GROUP BY parent, user_id
-  ORDER BY date_created DESC
+  LEFT JOIN vote_counts v ON cte.content_id = v.content_id
+  GROUP BY cte.parent
+  ORDER BY date_created DESC;  
   `;
   try {
     const result = await database.query(query);
@@ -96,31 +104,39 @@ SELECT
 const getPostsAndUserVotes = async (user_id) => {
   const query = `
   WITH RECURSIVE cte_posts AS 
-  (SELECT content_id, user_id, content, date_created, content_id AS parent
-  FROM contents WHERE parent_id IS NULL
-    UNION
-    SELECT c.content_id, c.user_id, c.content, c.date_created, cte.parent
-    FROM cte_posts cte
-    JOIN contents c ON cte.content_id = c.parent_id
+  (
+      SELECT content_id, user_id, content, date_created, content_id AS parent
+      FROM contents 
+      WHERE parent_id IS NULL
+      UNION
+      SELECT c.content_id, c.user_id, c.content, c.date_created, cte.parent
+      FROM cte_posts cte
+      JOIN contents c ON cte.content_id = c.parent_id
+  ), vote_counts AS
+  (
+      SELECT SUM(v.value) AS num_votes, cte.content_id
+      FROM cte_posts cte
+      LEFT JOIN votes v ON cte.content_id = v.content_id
+      GROUP BY cte.content_id
   )
-SELECT 
-    cte.content_id,
-    cte.user_id,
-    username,
-    profile_img,
-    title,
-    date_created,
-    content,
-    COUNT(*) - 1 AS num_comments,
-    IFNULL(SUM(v.value), 0) AS num_votes,
-    user_votes.vote_id,
-    user_votes.value AS vote_value
+  SELECT 
+      cte.content_id,
+      cte.user_id,
+      username,
+      profile_img,
+      title,
+      date_created,
+      content,
+      COUNT(*) - 1 AS num_comments,
+      IFNULL(vc.num_votes, 0) AS num_votes,
+      v.vote_id,
+      v.value
   FROM cte_posts cte
   JOIN users USING (user_id)
   LEFT JOIN posts p ON p.content_id = cte.parent
-  LEFT JOIN votes v ON v.content_id = cte.content_id
-  LEFT JOIN votes user_votes ON user_votes.user_id = :user_id AND user_votes.content_id = cte.content_id
-  GROUP BY parent, cte.user_id
+  LEFT JOIN vote_counts vc ON cte.content_id = vc.content_id
+  LEFT JOIN votes v ON cte.content_id = v.content_id AND v.user_id = 7
+  GROUP BY cte.parent
   ORDER BY date_created DESC;
   `;
   const params = {
