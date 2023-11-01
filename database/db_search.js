5e41664bee1a3a914de4cbeffd929ac24411bca2
Prince H Chabveka <pchabveka@my.bcit.ca>
2023-10-31 21:39:40 -0700

    remove unused function

--- database/db_search.js
+++ database/db_search.js
@@ -1,31 +1,6 @@
 const database = include("mySQLDatabaseConnection");
 
 
-const searchComments = async (keyword) => {
-    const query = `
-    SELECT 
-  text, 
-  MATCH(text) AGAINST (: keyword IN BOOLEAN MODE) as score 
-FROM 
-  comments 
-WHERE 
-  MATCH(text) AGAINST (: keyword IN BOOLEAN MODE) 
-ORDER BY 
-  score DESC;
-
-
-
-//     `;
-//     const params = { keyword: keyword };
-//     try {
-//       const result = await database.query(query, params);
-//       return result[0].length > 0;
-//     } catch (error) {
-//       console.error("Error while checking username:", error);
-//       return false;
-//     }
-//   };
-
 
 const searchComments = async (keyword) => {
     const query = `
