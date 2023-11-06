ac5509ab21cecd337c93bdb9547226d99263be92
Prince H Chabveka <pchabveka@my.bcit.ca>
2023-11-05 16:15:15 -0800

    update search

--- database/db_search.js
+++ database/db_search.js
@@ -2,12 +2,33 @@ const database = include("mySQLDatabaseConnection");
 
 
 
+// const searchComments = async (keyword) => {
+//     const query = `
+
+
+//     SELECT content
+//     FROM contents
+//     WHERE content LIKE '%' || :keyword || '%';    
+      
+//     `;
+//     const params = {
+//         keyword: keyword
+//     };
+//     try {
+//         const result = await database.query(query, params);
+//         return result[0].length > 0;
+//     } catch (error) {
+//         console.error("Error while checking username:", error);
+//         return false;
+//     }
+// };
+
+
 const searchComments = async (keyword) => {
     const query = `
     SELECT content
     FROM contents
-    WHERE content LIKE '%' || :keyword || '%';    
-      
+    WHERE MATCH(content) AGAINST (:keyword IN BOOLEAN MODE);
     `;
     const params = {
         keyword: keyword
@@ -20,6 +41,7 @@ const searchComments = async (keyword) => {
         return false;
     }
 };
+
 
 module.exports = {
     searchComments
