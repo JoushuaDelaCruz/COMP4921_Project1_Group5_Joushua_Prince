79d88602b46470c9d4805ff7f4a063ab9fd5dab2
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-06 00:18:18 -0800

    Created a favouriteRouter with addFavourite and deleteFavourite routers

---
+++ routers/favouriteRouter.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const db_favourites = include("database/db_favourites");
+
+router.post("/addFavourite", async (req, res) => {
+  const sessionID = req.body.data.sessionID;
+  req.sessionStore.get(sessionID, async (err, session) => {
+    if (err) {
+      console.error("Error while getting session:", err);
+      res.status(500).send(false);
+      return;
+    }
+    if (!session) {
+      res.status(401).send(false);
+      return;
+    }
+    if (!session.authenticated) {
+      res.status(401).send(false);
+      return;
+    }
+    const user_id = session.user;
+    const content_id = req.body.data.content_id;
+    const success = await db_favourites.addFavourite(user_id, content_id);
+    res.send(success);
+  });
+});
+
+router.post("/deleteFavourite", async (req, res) => {
+  const sessionID = req.body.data.sessionID;
+  req.sessionStore.get(sessionID, async (err, session) => {
+    if (err) {
+      console.error("Error while getting session:", err);
+      res.status(500).send(false);
+      return;
+    }
+    if (!session) {
+      res.status(401).send(false);
+      return;
+    }
+    if (!session.authenticated) {
+      res.status(401).send(false);
+      return;
+    }
+    const user_id = session.user;
+    const content_id = req.body.data.content_id;
+    const success = await db_favourites.deleteFavourite(user_id, content_id);
+    res.send(success);
+  });
+});
+
+module.exports = router;
