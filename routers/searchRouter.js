08d550ed63524781e3c631f90ed2e43a603adc3e
Prince H Chabveka <pchabveka@my.bcit.ca>
2023-11-08 15:49:04 -0800

    remove dead code

--- routers/searchRouter.js
+++ routers/searchRouter.js
@@ -6,38 +6,34 @@ router.get("/:text", async (req, res) => {
   const text = req.params.text;
 
   try {
+    // First, perform the search
     const results = await db_contents.search(text);
-    res.send(results);
+
+    if (results) {
+      // If search results are found, fetch parent IDs for each result
+      const resultsWithParentIds = await Promise.all(
+        results.map(async (result) => {
+          const parent = await db_contents.getCommentReplies(result.content_id);
+          if (parent) {
+            result.parent_id = parent;
+          }
+          return result;
+        })
+      );
+      console.log("Parents stuff " + resultsWithParentIds[0].parent_id);
+
+      res.send(resultsWithParentIds);
+    } else {
+      res.status(404).send({
+        message: "Not found"
+      });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).send({
-      message: "Internal server error",
+      message: "Internal server error"
     });
   }
-
-  //   if (results) {
-  //     const resultsWithParentIds = await Promise.all(
-  //       results.map(async (result) => {
-  //         const parent = await db_contents.getCommentReplies(result.content_id);
-  //         if (parent) {
-  //           result.parent_id = parent;
-  //         }
-  //         return result;
-  //       })
-  //     );
-  //     console.log("Parents stuff " + resultsWithParentIds[0].parent_id)
-  //     res.send(resultsWithParentIds);
-  //   } else {
-  //     res.status(404).send({
-  //       message: "Not found"
-  //     });
-  //   }
-  // } catch (error) {
-  //   console.error(error);
-  //   res.status(500).send({
-  //     message: "Internal server error"
-  //   });
-  // }
 });
 
 module.exports = router;
