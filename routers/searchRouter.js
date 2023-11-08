22dd2d18a60c604037d04b006d8e42b9cbf2b4f3
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-08 01:05:42 -0800

    Commented out the mapping for now

--- routers/searchRouter.js
+++ routers/searchRouter.js
@@ -7,30 +7,37 @@ router.get("/:text", async (req, res) => {
 
   try {
     const results = await db_contents.search(text);
-
-    if (results) {
-      const resultsWithParentIds = await Promise.all(
-        results.map(async (result) => {
-          const parent = await db_contents.getCommentReplies(result.content_id);
-          if (parent) {
-            result.parent_id = parent;
-          }
-          return result;
-        })
-      );
-      console.log("Parents stuff " + resultsWithParentIds[0].parent_id)
-      res.send(resultsWithParentIds);
-    } else {
-      res.status(404).send({
-        message: "Not found"
-      });
-    }
+    res.send(results);
   } catch (error) {
     console.error(error);
     res.status(500).send({
-      message: "Internal server error"
+      message: "Internal server error",
     });
   }
+
+  //   if (results) {
+  //     const resultsWithParentIds = await Promise.all(
+  //       results.map(async (result) => {
+  //         const parent = await db_contents.getCommentReplies(result.content_id);
+  //         if (parent) {
+  //           result.parent_id = parent;
+  //         }
+  //         return result;
+  //       })
+  //     );
+  //     console.log("Parents stuff " + resultsWithParentIds[0].parent_id)
+  //     res.send(resultsWithParentIds);
+  //   } else {
+  //     res.status(404).send({
+  //       message: "Not found"
+  //     });
+  //   }
+  // } catch (error) {
+  //   console.error(error);
+  //   res.status(500).send({
+  //     message: "Internal server error"
+  //   });
+  // }
 });
 
 module.exports = router;
