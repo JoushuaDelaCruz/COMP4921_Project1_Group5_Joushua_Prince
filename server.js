2f0ac8e377f6c7566e86d99f14224656f759e613
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-08 17:58:39 -0800

    Removed deadcode

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
-const path = require("path");
 const PORT = process.env.PORT || 5000;
 const app = express();
 const MongoStore = require("connect-mongo");
